c2734bf3052f32b703d5b4483426515f9cd7bc3f
Emily McAllister <esm328@nyu.edu>
2019-01-09 18:07:33 -0500

    tests written and passing

--- server/api/orders.js
+++ server/api/orders.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const order = await Order.create(req.body);
-    res.json(order);
+    res.status(201).json(order);
   } catch (err) {
     next(err);
   }
---
+++ server/api/orders.spec.js
@@ -0,0 +1,40 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai');
+const request = require('supertest');
+const db = require('../db');
+const app = require('../index');
+const Order = db.model('order');
+
+describe('Order routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true});
+  });
+
+  describe('/api/orders/', () => {
+    beforeEach(() => {
+      return Order.create({
+        chargeId: '1',
+        amount: 500,
+        items: [1, 2]
+      });
+    });
+
+    it('GET /api/orders', async () => {
+      const res = await request(app)
+        .get('/api/orders')
+        .expect(200);
+
+      expect(res.body).to.be.an('array');
+      expect(res.body[0].chargeId).to.be.equal('1');
+    });
+
+    it('POST /api/orders', async () => {
+      const data = {chargeId: 'hey', amount: 500, items: [1, 2]};
+      const res = await request(app)
+        .post('/api/orders/')
+        .send(data)
+        .expect(201);
+    });
+  });
+});
