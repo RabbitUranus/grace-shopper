49f4790a55859ff1deabb903cdb238abe4e06214
Guli <gulikholmatova@gmail.com>
2019-01-10 14:45:55 -0500

    added map function to the cart components and got cart functionality working

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-export default class Cart extends React.Component {
+export class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    console.log('hello');
+    console.log('props', this.props);
+    console.log('cart', Array.isArray(this.props.cart));
+    const {cart} = this.props;
+
     return (
       <table>
         <thead>
@@ -12,8 +19,26 @@ export default class Cart extends React.Component {
             <th>Quantity</th>
             <th>Total</th>
           </tr>
+          {cart &&
+            cart.map(product => (
+              <tr key={product.id}>
+                <th>{product.name}</th>
+                <th>{product.price}</th>
+                <th>1</th>
+                <th>{product.price}</th>
+              </tr>
+            ))}
         </thead>
       </table>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart.cart
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Cart);
