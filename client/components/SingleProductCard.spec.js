5b5601dfa842fee2f82696f11f8266ceae72b92b
Elcess <kelcess@alum.mit.edu>
2019-01-15 07:12:33 -0500

    remove unused import from test script

--- client/components/SingleProductCard.spec.js
+++ client/components/SingleProductCard.spec.js
@@ -5,7 +5,6 @@ import React from 'react';
 import enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {SingleProductCard} from './SingleProductCard';
-import sinon from 'sinon';
 
 const adapter = new Adapter();
 enzyme.configure({adapter});
