8a3686df126028e5073e2c311ba1af630009c98c
Guli <gulikholmatova@gmail.com>
2019-01-15 09:41:50 -0500

    installed query-string package

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchProducts} from '../reducers/products';
-
 import SingleProductCard from './SingleProductCard';
+import queryString from 'query-string';
 
 export class AllProducts extends Component {
   componentDidMount() {
-    this.props.fetchProducts();
+    let query;
+    console.log(this.props.location.search);
+    const values = queryString.parse(this.props.location.search);
+    console.log('values', values);
+    const category = values.category || '';
+    // const category = this.props.match.params.category || '';
+    console.log('category:', category);
+    if (!category) {
+      query = '';
+    } else {
+      query = `?category=${category}`;
+    }
+    console.log('query:', query);
+    this.props.fetchProducts(query);
   }
 
   render() {
@@ -31,7 +44,10 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchProducts: () => dispatch(fetchProducts())
+  fetchProducts: query => dispatch(fetchProducts(query))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(AllProducts);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AllProducts);
